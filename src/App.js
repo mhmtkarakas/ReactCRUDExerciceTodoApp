7b17e6d3761bd84c6cdf0b6ea42bfde2fd530648
mhmtkarakas <m.karakas32@gmail.com>
2024-01-27 19:52:06 +0300

    edit process carried out

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import React, { useState } from "react";
 function App() {
   const [todoText, setTodoText] = useState("");
   const [todos, setTodos] = useState([]);
+  // Edit Button
+  const [pressEditButton, setPressEditButton] = useState(false);
+  const [willEditText, setWillEditText] = useState('');
+  const [willEditTodo, setWillEditTodo] = useState(null );
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -23,26 +27,24 @@ function App() {
   };
   // Deleted Todo
   const deletedTodo = (id) => {
-    const filteredTodos = todos.filter(
-      (item) => item.id !== id
-    );
+    const filteredTodos = todos.filter((item) => item.id !== id);
     setTodos(filteredTodos);
-  }
-  // Change hasDone 
-  const changeHasDone = (item) =>{
-     console.log(item);
-     let tempTodos = [];
-     todos.map((i,index)=>{
-      if(i.id  === item.id){
-       let updatedTodo = {
-             ...item,
-             hasDone: !item.hasDone
-       }
-       tempTodos.push(updatedTodo);
-      }else{
-       tempTodos.push(i);
+  };
+  // Change hasDone
+  const changeHasDone = (item) => {
+    console.log(item);
+    let tempTodos = [];
+    todos.map((i, index) => {
+      if (i.id === item.id) {
+        let updatedTodo = {
+          ...item,
+          hasDone: !item.hasDone,
+        };
+        tempTodos.push(updatedTodo);
+      } else {
+        tempTodos.push(i);
       }
-     })
+    });
     //  for(let i = 0; i < todos.length; i++){
     //   if(todos[i].id===todo.id){
     //      let updatedTodo ={
@@ -54,9 +56,31 @@ function App() {
     //      tempTodos.push(todos[i])
     //   }
     //  }
-     setTodos(tempTodos);
-  }
+    setTodos(tempTodos);
+  };
 
+     // Edit Todos
+      const editTodo = (e)=>{
+        e.preventDefault();
+        if(willEditText=== ""){
+          alert('Please enter')
+          return
+        }
+        let tempTodos = [];
+        todos.map((todo)=>{
+          if(todo.id === willEditTodo.id){
+              let updatedTodo = {
+                ...willEditTodo,
+                title:willEditText
+              }
+              tempTodos.push(updatedTodo);
+          }else{
+             tempTodos.push(todo)
+          }
+        })
+        setTodos(tempTodos);
+        setPressEditButton(false); 
+      }
   return (
     <div className="container my-5">
       <form onSubmit={handleSubmit}>
@@ -73,6 +97,27 @@ function App() {
           </button>
         </div>
       </form>
+
+      {pressEditButton === true && (
+        <form onSubmit={editTodo}>
+          <div className="input-group mb-3">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Please Enter"
+              value={willEditText}
+              onChange={(event) => setWillEditText(event.target.value)}
+            />
+            <button onClick={()=>setPressEditButton(false)} className="btn btn-danger w-25" type="button">
+              Cancel
+            </button>
+            <button className="btn btn-primary w-25" type="submit">
+              Save
+            </button>
+          </div>
+        </form>
+      )}
+
       <div className="container">
         {todos.length === 0 ? (
           <p>You don't have any todos yet</p>
@@ -96,13 +141,30 @@ function App() {
                 </div>
                 <div>
                   <button
-                    onClick={() =>{deletedTodo(todo.id)}}
+                    onClick={() => {
+                      deletedTodo(todo.id);
+                    }}
                     className="btn btn-sm btn-danger"
                   >
                     Delete
                   </button>
-                  <button className="btn btn-sm btn-secondary">Edit</button>
-                  <button onClick = {()=>{changeHasDone(todo)}}className="btn btn-sm btn-success">
+                  <button
+                  onClick={() => {
+                    setPressEditButton(true);
+                    setWillEditText(todo.title);
+                    setWillEditTodo(todo);
+                  }}
+                    className="btn btn-sm btn-secondary"
+                  >
+                    Edit
+                  </button>
+                  <button
+                    onClick={() => {
+                      changeHasDone(todo);
+
+                    }}
+                    className="btn btn-sm btn-success"
+                  >
                     {todo.hasDone === true ? "Done" : "Undone"}
                   </button>
                 </div>
